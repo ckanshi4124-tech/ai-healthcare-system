4b0a9045f4749c3d320a79f38b869ff24542c7cb
ckanshi4124-tech <ckanshi4124@gmail.com>
2025-11-23 22:23:39 +0530

    Phase 1 Day 2: User model + migrations + cloud DB setup

---
+++ backend/app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from backend.app.database.db_setup import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default="patient")  # patient, doctor, admin
